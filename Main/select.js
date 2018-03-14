ebb5da70962944f672c79104201af6549ea0a6a3
maxim_mariukhin <maryuhin.z@gmail.com>
2018-03-14 22:34:40 +0200

    drag & drop

--- Main/select.js
+++ Main/select.js
@@ -1,3 +1,10 @@
+var mouse ={
+    x : 0,
+    y : 0
+};
+
+var selected = false;
+
 var cnv = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
@@ -47,10 +54,18 @@ var i = 0, rect = [];
 for(;i<5;i++){
     rect.push(new Rect(50 + i*(50+10),40,50,50));
 }
+//////////---Для подсветки при нажатии---//////////////
+// var isCursorInRect = function (x,y,rect) {
+//     return x > rect.x && x < rect.x + rect.w &&
+//         y >rect.y && y < rect.y + rect.h;
+// };
 
-var isCursorInRect = function (x,y,rect) {
-    return x > rect.x && x < rect.x + rect.w &&
-        y >rect.y && y < rect.y + rect.h;
+
+///////---Для подсветки при наведении---//////////
+var isCursorInRect = function (rect) {
+    return mouse.x > rect.x && mouse.x < rect.x +
+        rect.w && mouse.y > rect.y && mouse.y < rect.y +
+        rect.h;
 };
 
 cnv.onclick = function (e) {
@@ -68,12 +83,46 @@ setInterval(function () {
   for(i in rect){
       rect[i].draw();
 
-      if(rect[i].selected)
-        rect[i].stroke();
+      if(isCursorInRect(rect[i])){
+          rect[i].stroke();
+      }
+     /* if(rect[i].selected)
+        rect[i].stroke();*/
 
       // if(rect[2].selected) {
       //     alert("Третья кнопка");
       //     ctx.clearRect(0, 0, width, height);
       // }
   }
+
+  if(selected){
+      selected.x = mouse.x - selected.w/2;
+      selected.y = mouse.y - selected.w/2;
+  }
+
 },30);
+
+window.onmousemove = function (e) {
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
+};
+window.onmousedown = function (e) {
+    mouse.down = true;
+    if(!selected){
+        var i;
+        for(i in rect){
+            if(isCursorInRect(rect[i])){
+                selected = rect[i];
+            }
+        }
+    }
+};
+
+window.onmouseup = function () {
+    mouse.down = false;
+    selected = false;
+};
+
+
+
+
